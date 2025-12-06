2025501ed80f5d4aea8739eea6e45df1482702a1
Divya Renge <divyarenge1212@gmail.com>
2025-12-06 16:05:53 +0530

    Add files via upload

---
+++ Assignment-39/Asg39_4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+    char ch = '\0';
+
+    for(iCnt = iNo, ch = 'A'; iCnt >= 1; iCnt--)
+    {
+        printf("%c\t",ch);
+        ch++;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the Value: ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
